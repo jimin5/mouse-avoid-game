a6b135a5d8464b6a175da5214989551480a55000
jimin <jiminon5@gmail.com>
2018-01-11 20:38:36 +0900

    play

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const canvas = document.getElementById('panel');
 const context = canvas.getContext('2d');
 
+const src = 'resource/images/img_Character.png';
+const objectArray = [];
+let isPaused = false;
+let play = true;
+let score = 0;
+let time = 120;
+let level = 800;
+
+let player = new GameObject(2, 2, 65, 80);
+objectArray.push(player);
+
+player.x = canvas.width/2 - player.width/2;
+player.y = canvas.height/2 - player.height/2;
+//화면 아래에서 리스폰
+
 function GameObject(cx, cy, width, height)
 {
     //이미지를 자를떄 기준의 좌표값
@@ -10,10 +25,11 @@ function GameObject(cx, cy, width, height)
     this.x = 0;
     this.y = 0;
     this.image = new Image();
-    this.image.src = 'image.png';
+    this.image.src = 'resource/images/img_Character.png';
     this.width = width;
     this.height = height;
     this.direction = 0;
+    this.side = 0;
 
     this.isObstacle = false;//장애물인지 확인
     this.long = false;
@@ -26,55 +42,198 @@ function Rect(x, y, width, height) {
     this.height = height;
 }
 
-const spriteDefinition = {
-    MOUSE_UP: new Rect(0, 2, 70, 78),
-    MOUSE_DOWN:new Rect(70, 2, 70, 78),
-    MOUSE_RIGHT: new Rect(140, 2, 70, 71),
-    MOUSE_LEFT: new Rect(210, 2, 70, 71),
-    UP_HURT: new Rect(512, 2, 55, 78),
-    DOWN_HURT: new Rect(575, 2, 55, 78),
-    RIGHT_HURT: new Rect(107, 2, 70, 71),
-    LEFT_HURT: new Rect(293, 2, 70, 71),
-    FORK_UP: new Rect(14, 82, 21, 114),
-    FORK_DOWN: new Rect(164, 82, 21, 114),
-    FORK_RIGHT: new Rect(42, 114, 114, 21),
-    FORK_LEFT: new Rect(42, 90, 114, 21),
+const character = {
+    MOUSE_UP: new Rect(2, 2, 65, 80),
+    MOUSE_DOWN:new Rect(131, 2, 65, 80),
+    MOUSE_RIGHT: new Rect(260, 2, 67, 71),
+    MOUSE_LEFT: new Rect(409, 2, 67, 71),
+    UP_HURT: new Rect(67, 2, 64, 80),
+    DOWN_HURT: new Rect(196, 2, 64, 80),
+    RIGHT_HURT: new Rect(327, 2, 68, 74),
+    LEFT_HURT: new Rect(476, 2, 68, 74),
+    FORK_UP: new Rect(14, 82, 21, 115),
+    FORK_DOWN: new Rect(164, 82, 21, 115),
+    FORK_RIGHT: new Rect(42, 114, 115, 21),
+    FORK_LEFT: new Rect(42, 90, 115, 21),
     KNIFE_UP: new Rect(195, 97, 31, 98),
     KNIFE_DOWN: new Rect(338, 97, 31, 98),
-    KNIFE_RIGHT: new Rect(234, 122, 98, 31),
+    KNIFE_RIGHT: new Rect(236, 122, 98, 31),
     KNIFE_LEFT: new Rect(233, 90, 98, 31),
     CHEESE: new Rect(389, 90, 40, 32),
-    PEPPER: new Rect(449, 91, 27, 45)
+    PEPPER: new Rect(445, 91, 30, 45)
 }
 
-const src = 'image.png';
-const player = new GameObject(210, 2, 70, 71);
-//const obstacle = new GameObject(src, 60, 60);
-const objectArray = [];
-var isPaused = false;
-var play = true;
+function Knife(dir){
+  let newObstacle;
+    if(dir == 0){
+      newObstacle =
+      new GameObject(character.KNIFE_DOWN.x, character.KNIFE_DOWN.y,
+        character.KNIFE_DOWN.width, character.KNIFE_DOWN.height);
+      newObstacle.side = 0;
+    }
+    else if(dir == 1){
+      newObstacle =
+      new GameObject(character.KNIFE_LEFT.x, character.KNIFE_LEFT.y,
+        character.KNIFE_LEFT.width, character.KNIFE_LEFT.height);
+      newObstacle.side = 1;
+    }
+    else if(dir == 2){
+      newObstacle =
+      new GameObject(character.KNIFE_UP.x, character.KNIFE_UP.y,
+        character.KNIFE_UP.width, character.KNIFE_UP.height);
+      newObstacle.side = 2;
+    }
+    else{
+      newObstacle =
+      new GameObject(character.KNIFE_RIGHT.x, character.KNIFE_RIGHT.y,
+        character.KNIFE_RIGHT.width, character.KNIFE_RIGHT.height);
+      newObstacle.side = 3;
+    }
+    objectPush(newObstacle);
+}
 
-//화면 아래에서 리스폰
-player.x = canvas.width/2 - 30;
-player.y = canvas.height - 60;
+function Fork(dir){
+  let newObstacle;
+  if(dir == 0){
+    newObstacle =
+    new GameObject(character.FORK_DOWN.x, character.FORK_DOWN.y,
+      character.FORK_DOWN.width, character.FORK_DOWN.height);
+    newObstacle.side = 0;
+  }
+  else if(dir == 1){
+    newObstacle =
+    new GameObject(character.FORK_LEFT.x, character.FORK_LEFT.y,
+      character.FORK_LEFT.width, character.FORK_LEFT.height);
+    newObstacle.side = 1;
+  }
+  else if(dir == 2){
+    newObstacle =
+    new GameObject(character.FORK_UP.x, character.FORK_UP.y,
+      character.FORK_UP.width, character.FORK_UP.height);
+    newObstacle.side = 2;
+  }
+  else{
+    newObstacle =
+    new GameObject(character.FORK_RIGHT.x, character.FORK_RIGHT.y,
+      character.FORK_RIGHT.width, character.FORK_RIGHT.height);
+    newObstacle.side = 3;
+  }
+  objectPush(newObstacle);
+}
 
-objectArray.push(player);
-let score = 0;
-let time = 120;
+function objectPush(newObstacle){
+  objectArray.push(newObstacle);
+  newObstacle.isObstacle = true;
+  newObstacle.direction = parseInt(Math.random()*3);
+  const rx = Math.random() * (canvas.width-newObstacle.width-2);
+  //리스폰되는 x좌표
+  const ry = Math.random() * (canvas.height-newObstacle.height-2);
+  //리스폰되는 y좌표
+  appearSide(rx, ry, newObstacle);
+}
+
+function appearSide(rx, ry, newObstacle){
+  const n = newObstacle;
+  if(!n.side){
+    n.x = rx;
+    n.y = -n.height;
+  }
+  else if(n.side == 1){
+    n.x = canvas.width;
+    n.y = ry;
+  }
+  else if(n.side == 2){
+    n.x = rx;
+    n.y = canvas.height;
+  }
+  else{
+    n.x = -n.width;
+    n.y = ry;
+  }
+}
+
+function down(obj){
+  if(obj.x < 0) obj.direction = 0;
+  else if(obj.x > canvas.width)obj.direction = 1;
+  //벽에 맞으면 튕겨내기
+
+  if(obj.direction == 0){
+      obj.x += 5;
+      obj.y += 5;
+  }
+  else if(obj.direction == 1){
+      obj.x -= 5;
+      obj.y += 5;
+  }
+  else if(obj.direction == 2){
+      obj.y += 5;
+  }
+}
+
+function up(obj){
+  if(obj.x < 0) obj.direction = 0;
+  else if(obj.x > canvas.width)obj.direction = 1;
+  //벽에 맞으면 튕겨내기
+
+  if(obj.direction == 0){
+      obj.x += 5;
+      obj.y -= 5;
+  }
+  else if(obj.direction == 1){
+      obj.x -= 5;
+      obj.y -= 5;
+  }
+  else if(obj.direction == 2){
+      obj.y -= 5;
+  }
+}
+
+function left(obj){
+  if(obj.y < 0) obj.direction = 0;
+  else if(obj.y > canvas.height)obj.direction = 1;
+  //벽에 맞으면 튕겨내기
+
+  if(obj.direction == 0){
+      obj.x -= 5;
+      obj.y += 5;
+  }
+  else if(obj.direction == 1){
+      obj.x -= 5;
+      obj.y -= 5;
+  }
+  else if(obj.direction == 2){
+      obj.x -= 5;
+  }
+}
+
+function right(obj){
+  if(obj.y < 0) obj.direction = 0;
+  else if(obj.y > canvas.height)obj.direction = 1;
+  //벽에 맞으면 튕겨내기
+
+  if(obj.direction == 0){
+      obj.x += 5;
+      obj.y += 5;
+  }
+  else if(obj.direction == 1){
+      obj.x += 5;
+      obj.y -= 5;
+  }
+  else if(obj.direction == 2){
+      obj.x += 5;
+  }
+}
 
 // 1초마다 한번씩 실행
 setInterval(function() {
     if(isPaused) play = false;
     if(play){
-      const newObstacle = new GameObject(445, 91, 30, 45);
-      objectArray.push(newObstacle);
-      newObstacle.isObstacle = true;
-
-      newObstacle.direction = parseInt(Math.random()*3);
-      newObstacle.x = Math.random() * 440;//0에서 440사이의 소수 반환
-      newObstacle.y = -newObstacle.height;
+      let n = parseInt(Math.random()*2);
+      let dir = parseInt(Math.random()*4);
+      if(n) Knife(dir);
+      else Fork(dir);
     }
-}, 1000);
+}, level*=(9/10));
 
 const downKeys = {};
 
@@ -125,13 +284,13 @@ function run()
         if (obj === player) {
             //사용자 사라짐 방지
             if(obj.x < 0){
-                obj.x = canvas.width;
+                obj.x = canvas.width-obj.width;
             }
             else if(obj.x > canvas.width){
                 obj.x = 0;
             }
             if(obj.y < 0){
-                obj.y = canvas.height;
+                obj.y = canvas.height-obj.height;
             }
             else if(obj.y > canvas.height){
                 obj.y = 0;
@@ -140,35 +299,20 @@ function run()
         }
 
         if (obj.isObstacle) {
-            //장애물 여러방향에서 내려오기
-            if(obj.direction == 0){
-                obj.x += 5;
-                obj.y += 5;
-            }
-            else if(obj.direction == 1){
-                obj.x -= 5;
-                obj.y += 5;
-            }
-            else if(obj.direction == 2){
-                obj.y += 5;
-            }
+            //장애물 여러방향에서 내려오기(장애물 이동방향 기준)
+            if(!obj.side) down(obj);
+            else if(obj.side == 1) left(obj);
+            else if(obj.side == 2) up(obj);
+            else right(obj);
+
             if(obj.long) longer(player, obj);
             else if(obj.long == false) shorter(player, obj);
         }
 
-        if(obj.y == canvas.height){ //canvas.height로 게임화면 세로사이즈 가져옴
-            ++score;
-        }
-
-        context.font = "20px malgun gothic"; //폰트의 크기, 글꼴체 지정
-        context.fillStyle = "black"; //색상지정
-        context.fillText("score : "+score,canvas.width-100,30); //점수를 지정한 위치에 찍어준다.
-        context.fill();
-
+        if(checkCollision(player, obj)) gameover = true;
     }
 
     window.addEventListener('blur', function(e) {
-      console.log(a);
       e.preventDefault();
       isPaused = true;
     });
@@ -178,14 +322,34 @@ function run()
       isPaused = false;
     });
 
-    if (downKeys['ArrowLeft'])
+    if (downKeys['ArrowLeft']){
         player.x -= 10;
-    if (downKeys['ArrowRight'])
+        player.cx = character.MOUSE_LEFT.x;
+        player.cy = character.MOUSE_LEFT.y;
+        player.width = character.MOUSE_LEFT.width;
+        player.height = character.MOUSE_LEFT.height;
+      }
+    if (downKeys['ArrowRight']){
         player.x += 10;
-    if (downKeys['ArrowUp'])
+        player.cx = character.MOUSE_RIGHT.x;
+        player.cy = character.MOUSE_RIGHT.y;
+        player.width = character.MOUSE_RIGHT.width;
+        player.height = character.MOUSE_RIGHT.height;
+      }
+    if (downKeys['ArrowUp']){
         player.y -= 10;
-    if (downKeys['ArrowDown'])
+        player.cx = character.MOUSE_UP.x;
+        player.cy = character.MOUSE_UP.y;
+        player.width = character.MOUSE_UP.width;
+        player.height = character.MOUSE_UP.height;
+      }
+    if (downKeys['ArrowDown']){
         player.y += 10;
+        player.cx = character.MOUSE_DOWN.x;
+        player.cy = character.MOUSE_DOWN.y;
+        player.width = character.MOUSE_DOWN.width;
+        player.height = character.MOUSE_DOWN.height;
+      }
 
     window.requestAnimationFrame(run);
 }
@@ -211,5 +375,3 @@ function shorter(player, obj){
         if(time <= 0)gameover = true;
     }
 }
-
-//time -= 1;
